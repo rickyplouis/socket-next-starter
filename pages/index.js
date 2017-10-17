df6dcb75d53ba7d92979b587369f9ba98a9a471e
Ricardo <ricardoplouis@gmail.com>
2017-10-17 16:25:08 -0500

    Dynamically adds agenda items

--- pages/index.js
+++ pages/index.js
@@ -25,10 +25,21 @@ class HomePage extends Component {
       rooms: this.props.rooms,
       inputName: '',
       inputPassword: '',
-
+      inputMinutes: 0,
+      inputAgenda: [
+        { title: "",
+          message: {
+            speaker: '',
+            description: '',
+            duration: '',
+            startTime: ''
+          }
+        }
+      ]
     }
     this.handleAdmin = this.handleAdmin.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
   }
 
   // connect to WS server and listen event
@@ -52,6 +63,42 @@ class HomePage extends Component {
     this.setState({ inputName: event.target.value })
   }
 
+  handlePassword = event => {
+    this.setState({ inputPassword: event.target.value })
+  }
+
+  handleDuration = event => {
+    this.setState({ inputMinutes: event.target.value })
+  }
+
+  addAgendaItem = event => {
+    this.setState( state => ({
+      rooms: state.rooms,
+      inputName: state.inputName,
+      inputPassword: state.inputPassword,
+      inputMinutes: state.inputMinutes,
+      inputAgenda: state.inputAgenda.concat({
+        title: "",
+          message: {
+            speaker: '',
+            description: '',
+            duration: '',
+            startTime: ''
+          }
+        })
+    }))
+  }
+
+  clearForm(){
+    this.setState(state => ({
+      rooms: state.rooms.concat(room),
+      inputName: '',
+      inputPassword: '',
+      inputMinutes: 0,
+      inputAgenda: []
+    }))
+  }
+
   // send messages to server and add them to the state
   handleSubmit = event => {
     event.preventDefault()
@@ -66,27 +113,36 @@ class HomePage extends Component {
       }
     }
 
-    console.log('this.state.inputName', this.state.inputName);
     // create message object
     const room = {
       id: uuidv1(),
       createdAt: new Date(),
       admin: this.state.inputName,
       password: this.state.inputPassword,
+      duration: this.state.inputMinutes,
       agenda: [] //array of agendaItems
     }
 
     // send object to WS server
     this.socket.emit('message', room)
+    this.clearForm();
+  }
 
-    // add it to state and clean current input value
-    this.setState(state => ({
-      rooms: state.rooms.concat(room)
-    }))
+  renderAgenda(){
+    let index = 0;
+    console.log('inputAgenda', this.state.inputAgenda);
+    let agenda = this.state.inputAgenda;
+    let agendaItems = agenda.map( (item) =>
+      <form key={index++}>
+        Some item
+        <input placeholder="Some item" type="text"/>{item.title}
+      </form>
+    )
+    return agendaItems
   }
 
   disableSubmit(){
-    return this.state.inputName.length == 0;
+    return this.state.inputName.length == 0 || this.state.inputPassword.length == 0 || this.state.inputMinutes == 0;
   }
 
   render () {
@@ -99,9 +155,12 @@ class HomePage extends Component {
                 <strong>id:</strong> {room.id}<br/>
                 <strong>admin:</strong> {room.admin}<br/>
                 <strong>password:</strong> {room.password}<br/>
+                <strong>duration:</strong> {room.duration}<br/>
               </div>
             )}
           </ul>
+          {this.renderAgenda()}
+          <button onClick={(e) => this.addAgendaItem()}>Add Agenda Item</button>
           <form onSubmit={this.handleSubmit}>
             <input
               onChange={this.handleAdmin}
@@ -109,6 +168,19 @@ class HomePage extends Component {
               placeholder='Enter Your Name'
               value={this.state.inputName}
             />
+            <input
+              onChange={this.handlePassword}
+              type='text'
+              placeholder='Enter Your Room Password'
+              value={this.state.inputPassword}
+            />
+            <input
+              onChange={this.handleDuration}
+              type='number'
+              placeholder='Enter Duration'
+              value={this.state.inputMinutes}
+            />
+
           <button disabled={this.disableSubmit()}>Send</button>
           </form>
         </div>
