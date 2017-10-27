3f0fbbe79b03724a675a4b70cf1893150e58be9d
Ricardo <ricardoplouis@gmail.com>
2017-10-27 10:59:44 -0500

    Refactoring parts of timer component

--- components/timer.js
+++ components/timer.js
@@ -3,10 +3,15 @@ export default class Timer extends React.Component {
   constructor() {
     super();
     this.state = {
-      time: {},
-      seconds: 59,
+      timeObject: {
+        minutes: 2,
+        seconds: 5
+      },
+      inputMins: 0,
+      inputSeconds: 0,
       initialTime: 0,
       percent: 100,
+      timerRunning: false
     };
 
     this.timer = 0;
@@ -15,86 +20,113 @@ export default class Timer extends React.Component {
     this.pauseTimer = this.pauseTimer.bind(this);
   }
 
-  secondsToTime(secs){
-    let hours = Math.floor(secs / (60 * 60));
-
-    let divisor_for_minutes = secs % (60 * 60);
-    let minutes = Math.floor(divisor_for_minutes / 60);
-
-    let divisor_for_seconds = divisor_for_minutes % 60;
-    let seconds = Math.ceil(divisor_for_seconds);
-
-    let obj = {
-      "h": hours,
-      "m": minutes,
-      "s": seconds
-    };
-    return obj;
-  }
-
-  componentDidMount() {
-    let timeLeftVar = this.secondsToTime(this.state.seconds);
-    this.setState({ time: timeLeftVar });
+  convertTimeToSeconds = (timeObject) => {
+    return timeObject.minutes * 60 + timeObject.seconds
   }
 
   startTimer() {
-    if (this.state.seconds > 0){
+    if (this.state.seconds > 0 && !this.state.timerRunning){
       this.timer = setInterval(this.countDown, 1000);
+      this.setState({
+        timerRunning: true
+      })
     }
   }
 
-  countDown() {
-    if (this.state.initialTime == 0){
-      this.setState({
-        initialTime: this.state.seconds
-      })
-    }
-    let seconds = this.state.seconds - 1;
+  componentWillMount(){
+    this.setState({
+      initialTime: this.convertTimeToSeconds(this.state.timeObject),
+      seconds: this.convertTimeToSeconds(this.state.timeObject)
+    })
+  }
 
-    if (seconds == 0) {
+  countDown() {
+    let seconds = this.state.seconds - 1;
+    if (seconds <= 0) {
       this.pauseTimer();
     }
 
     // Remove one second, set state so a re-render happens.
     this.setState({
-      time: this.secondsToTime(seconds),
       seconds: seconds,
       percent: (seconds / this.state.initialTime) * 100
     });
-
   }
 
   pauseTimer(){
     clearInterval(this.timer);
+    this.setState({
+      timerRunning: false
+    })
   }
 
-  displaySeconds(secs){
-    return secs < 10 ? '0' + secs : secs;
+  displaySeconds(totalSeconds){
+    let seconds = totalSeconds % 60;
+    return seconds < 10 ? '0' + seconds : seconds;
   }
 
-  renderTimeInput(){
+  displayMinutes(seconds){
+    let mins = Math.floor(seconds / 60);
+    return mins;
+  }
+
+  handleMins = (event) => {
+    event.preventDefault();
+    this.setState({
+      inputMins: parseInt(event.target.value)
+    })
+  }
+
+  handleSeconds = (event) => {
+    event.preventDefault();
+    this.setState({
+      inputSeconds: parseInt(event.target.value)
+    })
+  }
+
+  setTimer = () => {
+    let totalSeconds = this.state.inputSeconds + this.state.inputMins * 60;
+    this.setState({
+      initialTime: totalSeconds,
+      seconds: totalSeconds
+    })
+  }
+
+  renderTimeInput = () => {
     return (
       <div>
-        <Input labelPosition='left' type='number' placeholder='Mins'>
+        <Input labelPosition='left' type='number' placeholder='Mins' onChange={(e) => this.handleMins(e)}>
           <Label basic>Minutes</Label>
           <input />
         </Input>
-        <Input labelPosition='left' type='number' placeholder='Seconds'>
+        <Input labelPosition='left' type='number' placeholder='Seconds' onChange={(e) => this.handleSeconds(e)}>
           <Label basic>Seconds</Label>
           <input />
         </Input>
+        <Button onClick={this.setTimer}>Set Timer</Button>
       </div>
     )
   }
 
-  //TODO: Input doesn't work so need to incorporate that into the state
+  renderTimerControls = () => {
+    if (this.state.timerRunning){
+      return (
+        <Button onClick={this.pauseTimer} color='red'>Pause</Button>
+      )
+    } else {
+      return (
+        <Button onClick={this.startTimer} color='blue'>Start</Button>
+      )
+    }
+  }
+
   render() {
     return(
-      <div >
-        <Header as='h4'>Time Remaining: {this.state.m || 0}:{this.displaySeconds(this.state.time.s)}</Header>
+      <div>
+        {this.renderTimeInput()}
+        <Header as='h4'>Time Remaining: {this.displayMinutes(this.state.seconds)}:{this.displaySeconds(this.state.seconds)}</Header>
         <Progress percent={this.state.percent} indicating size={'tiny'} style={{width: '50vw'}} />
-        <Button onClick={this.startTimer} color='blue'>Start</Button>
-        <Button onClick={this.pauseTimer} color='red'>Pause</Button>
+        {this.renderTimerControls()}
       </div>
     )
   }
